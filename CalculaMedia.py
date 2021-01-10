25dfe8778595ff7fcbdf05a8ea8fbca81f13a88f
Castrorcs <65196648+Castrorcs@users.noreply.github.com>
2021-01-09 22:48:04 -0300

    Add files via upload

---
+++ CalculaMedia.py
@@ -0,0 +1,23 @@
+nota1 = 7
+nota2 = 8
+
+#Definição da média da escola
+def verificar_aprovacao():
+  media = calcular_media([nota1, nota2])
+
+  if media >= 6:
+    print('Você foi aprovado. Parabéns!')
+  else:
+    print('Você foi reprovado. Procure seu professor.')
+
+#Definindo como calcular média
+def calcular_media(notas):
+  quantidade = len(notas)
+
+  soma = 0
+  for nota in notas:
+    soma = soma + nota
+  media = soma / quantidade
+  return media
+#Verificando aprovação
+verificar_aprovacao()
